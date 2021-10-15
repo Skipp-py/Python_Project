6b914b7181aa0f0b34b416e5e143176e0f8a21d2
Skippy <skippy.coding@gmail.com>
2021-10-15 14:55:10 -0400

    Updated functionality of main and collector

--- collector.py
+++ collector.py
@@ -13,33 +13,38 @@ def collect_href():
 
     last_height = driver.execute_script("return document.body.scrollHeight")
 
-    next_button = driver.find_element_by_xpath("//button[@class='_pmpl8qu']")
-
     while True:
 
-        wait_time = random.randrange(2, 4)
-
-        time.sleep(wait_time)
-
-        # Scroll down to bottom
-        driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
-
         try:
-            time.sleep(wait_time)
-            next_button.click()
+            load_button = driver.find_element_by_xpath(
+                "//button[@class='_pmpl8qu']")
+        except:
+            continue
         finally:
-            # Wait to load page
+            wait_time = random.randrange(2, 4)
+
             time.sleep(wait_time)
 
-            # Calculate new scroll height and compare with last scroll height
-            new_height = driver.execute_script(
-                "return document.body.scrollHeight")
-            if new_height == last_height:
-                elems = driver.find_elements_by_xpath("//a[@class='_1jhvjuo']")
-                for ele in elems:
-                    page_hrefs.append(ele.get_attribute('href'))
-                break
-            last_height = new_height
+            # Scroll down to bottom
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+
+            try:
+                time.sleep(wait_time)
+                load_button.click()
+            finally:
+                # Wait to load page
+                time.sleep(wait_time)
+
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script(
+                    "return document.body.scrollHeight")
+                if new_height == last_height:
+                    elems = driver.find_elements_by_xpath(
+                        "//a[@class='_1jhvjuo']")
+                    for ele in elems:
+                        page_hrefs.append(ele.get_attribute('href'))
+                    break
+                last_height = new_height
 
     return page_hrefs
--- main.py
+++ main.py
@@ -5,21 +5,45 @@ import re
 import random
 import collector
 
+# Launch driver and open site
 collector.driver.get('https://www.airbnb.com/s/experiences/online')
 
-category_button = collector.driver.find_element_by_xpath(
-    "//span[@aria-label='Family friendly']")
+time.sleep(2)
 
-try:
-    time.sleep(2)
-    category_button.click()
-except:
-    time.sleep(2)
-    category_button.click()
+# Maximize the window after page loads
+collector.driver.maximize_window()
 
-time.sleep(4)
-good_for_groups = collector.collect_href()
+category_buttons = collector.driver.find_elements_by_xpath(
+    "//button[@class='_1n56hy1']")
 
-print(good_for_groups[:5])
+next_button = collector.driver.find_element_by_xpath(
+    "//button[@aria-label='Next' and @class='_137uqvg']")
 
+d = {}
+
+for i in range(15, 17):
+    try:
+        time.sleep(2)
+        category_buttons[i].click()
+    except:
+        try:
+            time.sleep(2)
+            category_buttons[i].click()
+        except:
+            time.sleep(2)
+            next_button.click()
+            time.sleep(2)
+            category_buttons[i].click()
+    finally:
+        time.sleep(2)
+        lst = collector.collect_href()
+        d[category_buttons[i].text] = lst[:5]
+        collector.driver.execute_script("window.history.go(-1)")
+
+# time.sleep(4)
+# good_for_groups = collector.collect_href()
+
+# print(good_for_groups[:5])
+
+print(d.items())
 collector.driver.close()
