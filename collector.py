1c88a12a12dc831634f1ecefed202cd727ad9b3b
Skippy <skippy.coding@gmail.com>
2021-10-14 23:39:44 -0400

    Added functionality to collector

--- collector.py
+++ collector.py
@@ -5,12 +5,17 @@ import re
 import random
 
 driver = webdriver.Chrome('/Users/skippy/Desktop/NYCDSA/chromedriver')
-
 driver.get('https://www.airbnb.com/s/experiences/online')
 
+element = driver.find_element_by_xpath("//button[@class='_pmpl8qu']")
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
 
 group_button = driver.find_element_by_xpath(
-    "//span[@aria-label='Great for groups']")
+    "//span[@aria-label='Family friendly']")
+
+page_hrefs = []
 
 try:
     time.sleep(2)
@@ -19,15 +24,9 @@ except:
     time.sleep(2)
     group_button.click()
 
-element = driver.find_element_by_xpath("//button[@class='_pmpl8qu']")
-
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-
 while True:
 
-    wait_time = random.randrange(2, 5)
+    wait_time = random.randrange(2, 4)
 
     time.sleep(wait_time)
 
@@ -44,7 +43,12 @@ while True:
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
+            elems = driver.find_elements_by_xpath("//a[@class='_1jhvjuo']")
+            for ele in elems:
+                page_hrefs.append(ele.get_attribute('href'))
             break
         last_height = new_height
 
+print(page_hrefs[:5])
+print(len(page_hrefs))
 driver.close()
